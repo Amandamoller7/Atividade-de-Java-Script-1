f75535cdf603c6931b6ca0e20a059e2c94d123ef
Amanda <amanda_moller@estudante.sesisenai.org.br>
2025-05-14 01:32:42 -0300

    add- lista 2

--- script.js
+++ script.js
@@ -125,6 +125,7 @@ function verificarPalindromo (string) {
         console.log('Essa palavra é um Palíndromo');
     }else {
         console.log('Essa palavra não é um Palíndromo');
+
     }
 }
 
@@ -139,3 +140,915 @@ function verificarPalindromo (string) {
 //mensagemPersonalizada(nome,mensagem);
 //mediatotal(n1,n2,n3);
 //verificarPalindromo (string);
+
+
+while (palpite !== numeroSecreto);
+
+//Funções;
+//Função 1;
+/*
+let n1= parseFloat (prompt ("Digite seu numero")); //Número 1;
+
+
+ function verificarPar (n1){  
+    if (n1 % 2 === 0){
+          console.log(n1 + "é igual a par.");
+ 
+     } else {
+         console.log(n1 + "é igual a impar.");
+     }
+  }
+  */
+ /*
+//função 2;
+let n1= parseFloat (prompt ("Digite seu numero")); //Número 1;
+let n2 =parseFloat (prompt  ("Digite o segundo numero")); //Número2;
+ function calcular(n1,n2) {  
+    const  soma = n1+n2;
+    const subtracao = n1-n2;
+    const multiplicacao = n1 * n2;
+    console.log("A soma é" + soma);
+    console.log("A subtração é " + subtracao);
+    console.log("A multiplicação é " + multiplicacao);
+    if  (n2 === 0 ) {                                                   //Utilização de IF para caso o numero seja zero;
+        console.log ("Não da pra dividir por zero");
+
+
+    } else {
+        const divisao = n1 / n2;
+        console.log("A divisão é " + divisao);
+    }
+   }
+   */
+//Atividade 3;
+ /*
+ let n = 10 //I é uma contadora, neste caso o valor dela é 10;
+    while (n >= 1)
+     {            //enquanto, condição ( contadora for maior que 1)  
+        do  {
+        console. log ("contando..." + n);   //contando o número, (contando 1, contando 2)
+        n--;   // O numero vai diminuindo
+        } while (n >= 0);
+    }
+    */
+
+
+//Atividade 4;
+   /*
+   let palavra = prompt ("Digite uma palavra");//guardar as palavras;
+   //  function inverterTexto (palavra) {
+        return palavra.split ('').reverse().join('');  
+    }
+    */
+//Atividade 5 ;
+ // variável para contar caracteres
+ /*
+let palavra = prompt ("digite uma palavra")//variável paara guardar palavra
+  function contartexto(palavra){
+   let nletras=palavra.length   // variável para contar caracteres                          //usando uma "palavra-chave" que conta strings;
+    console.log (nletras);
+  }
+    */
+
+
+//Atividade 6
+ /*
+const carro = {          //declarando um objeto.
+    marca:'Volkswargen ',
+    modelo:'Esmerald',                                   //preenchendo com os dados do novo fusca 2025
+    ano :'2025'
+   
+};
+
+
+
+
+console.log('O carro é da ' +carro.marca + ' Modelo '+ carro.modelo + ' lançado no ano '+ carro.ano)
+ */
+
+
+//Atividade 7;
+/*
+let mensagem= 'Olá!';
+let nome =  prompt ("Qual é o seu nome?");
+function mensagemPersonalizada(nome,mensagem ){
+    console.log(mensagem + nome);
+
+
+
+
+
+
+}
+*/
+// Atividade 8.
+/*
+let n1 =  parseFloat( prompt ("Digite um numero"));  
+let n2 = parseFloat(prompt ("Digite o segundo numero"));
+let n3 = parseFloat(prompt ("Digite o terceiro"));
+                                   
+ function mediatotal(n1, n2, n3){
+    const media=((n1+n2+n3)/3)  //Criando uma variavel que vai somar todos os numeros e dividir  para criar uma média.
+    console.log("A média é " + media);
+ }
+*/
+
+
+// Atividade 9
+/*
+let i = 21 // variável indicadora
+for (;i > 0;){   //para numero maior que zero
+    i--;    // Fechando o código
+    if ( i%3 == 0) {           //se esse número for divisivel (igual a zero) então
+        if (i === 0){              //se o número for 0 é igual, ele não pode ser divisivel por 3
+            console.log(i + " Esse número não é multiplo de 3");    
+        }else{                       // caso o número não seja zero
+            console.log( i + " Esse número aqui é múltiplo por 3");
+        }
+    } else {
+    console.log (i + " Esse número não é multiplo por 3" );     //caso o número não seja divisivel por 3
+}
+} */
+
+
+//Atividade 10
+/*let string = prompt('Digite uma palavra: ')
+
+
+function verificarPalindromo (string) {
+    let reverse = string.split('').reverse('').join('');
+    if (string === reverse) {
+        console.log('Essa palavra é um Palíndromo');
+    }else {
+        console.log('Essa palavra não é um Palíndromo');
+    }
+}
+
+
+*/
+
+
+
+
+//chamar função
+ //verificarPar(n1);
+ //calcular(n1,n2);
+ //inverterTexto(palavra);
+ //contartexto(palavra);
+//mensagemPersonalizada(nome,mensagem);
+//mediatotal(n1,n2,n3);
+//verificarPalindromo (string);
+
+
+
+
+//Lista 2
+
+
+/*
+11.
+
+
+
+
+let n = prompt('Digite um número: ')
+
+
+function verificarSinal(n) {
+    if (n > 0) {
+        console.log('Positivo')
+    } else if (n < 0) {
+        console.log('Negativo')
+    } else if (n == 0) {
+        console.log('Zero')
+    }
+}
+
+
+*/
+
+
+/*
+12.
+
+
+
+
+let temp = parseFloat(prompt('Digite uma temperatura em Celcius'))
+
+
+function converterTemperatura(temp) {
+    const conversao = (temp * 1.8) + 32
+    console.log('A conversão dessa temperatura para Fahrenheit é: ' + conversao)
+}
+
+
+
+
+*/
+
+
+/*
+13.
+
+
+let soma = 0;
+let n = 1;
+
+
+while (n < 101) {
+    soma += n;
+    n++;
+}
+
+
+console.log("A somatória de 1 a 100 é: " + soma);
+
+
+*/
+
+
+/*
+14.
+
+
+let string = prompt('Digite uma frase que contenha a palavra azul: ')
+
+
+function substituirPalavra(string) {
+    let novaString = string.replace('azul', 'vermelho');
+    console.log(novaString)
+}
+
+
+
+
+
+
+*/
+
+
+/*
+15.
+
+
+let string = prompt('Digite uma palavra: ')
+
+
+function verificarTamanho(string) {
+    let qntdLetras = string.length;
+    if (qntdLetras > 10) {
+        console.log('Essa palavra tem mais de 10 caracteres')
+    } else {
+        console.log('Essa palavra não tem mais de 10 caracteres')
+    }
+}
+
+
+*/
+
+
+/*
+16.
+
+
+let frutas = ["Maça", "Uva", "Banana", "Laranja", "Mamão"];
+
+
+console.log(frutas[2]);
+
+
+*/
+
+
+/*
+17.
+
+
+
+
+let nome = prompt('Digite seu nome: ');
+let saudacao = 'Bem vindo(a)! '
+
+
+function saudacaoo(nome, saudacao) {
+    console.log(saudacao + nome);
+}
+
+
+
+
+*/
+
+
+/*
+18.
+
+
+let nota = prompt('Digite sua nota: ')
+
+
+function verificarAprovacao(nota) {
+    if (nota >= 6) {
+        console.log('Você está Aprovado!')
+    } else {
+        console.log('Você está Reprovado.')
+    }
+}
+
+
+
+
+*/
+
+
+/*
+19.
+
+
+for (let i = 0; i <= 30; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
+
+
+*/
+
+
+/*
+20.
+
+
+let string = prompt('Digite uma palavra: ')
+let i = 0
+let vogais = 'aeiouAEIOU'
+
+
+function contarVogais(string) {
+    for (let letra of string) {
+        if (vogais.includes(letra)) {
+            i++
+        }
+    }
+    console.log(i)
+}
+
+
+*/
+
+
+/*
+21.
+
+
+const n1 = parseFloat(prompt('Digite seu primeiro número: '));
+const n2 = parseFloat(prompt('Digite seu segundo número: '));
+const n3 = parseFloat(prompt('Digite seu terceiro número: '));
+let notas = [n1, n2, n3];
+
+
+let somaDasNotas = 0;
+
+
+function somarArray(notas) {
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i];
+    }
+    console.log(somaDasNotas)
+}
+
+
+*/
+
+
+/*
+22.
+
+
+
+
+let n = parseFloat(prompt('Digite um número: '));
+
+
+function dobrarNumero(n) {
+    dobro = n + n;
+    console.log('O dobro desse número é: ' + dobro);
+}
+
+
+*/
+
+
+/*
+23.
+let n = 2
+
+
+while (n <= 20) {
+    n++
+    if (n % 2 === 0) {
+        console.log(n)
+    }
+}
+*/
+
+
+/*
+24.
+
+
+let string = prompt('Digite uma palavra: ');
+
+
+function capitalizar(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+console.log(capitalizar(string))
+
+
+*/
+
+
+/*
+25.
+let ano = prompt('Digite um ano: ');
+
+
+function anoBissexto(ano) {
+    if (ano % 4 === 0) {
+        console.log('Esse é um ano Bissexto');
+    } else {
+        console.log('Esse não é um ano Bissexto');
+    }
+}
+
+
+*/
+
+
+//chamar função
+//verificarSinal(n);
+//converterTemperatura(temp);
+//anoBissexto(ano);
+//substituirPalavra(string);
+//saudacaoo(nome, saudacao);
+//verificarAprovacao(nota);
+//contarVogais(string);
+//somarArray(notas);
+//dobrarNumero(n);
+//capitalizar(string);
+//Funções;
+//Função 1;
+/*
+let n1= parseFloat (prompt ("Digite seu numero")); //Número 1;
+
+
+ function verificarPar (n1){  
+    if (n1 % 2 === 0){
+          console.log(n1 + "é igual a par.");
+ 
+     } else {
+         console.log(n1 + "é igual a impar.");
+     }
+  }
+  */
+ /*
+//função 2;
+let n1= parseFloat (prompt ("Digite seu numero")); //Número 1;
+let n2 =parseFloat (prompt  ("Digite o segundo numero")); //Número2;
+ function calcular(n1,n2) {  
+    const  soma = n1+n2;
+    const subtracao = n1-n2;
+    const multiplicacao = n1 * n2;
+    console.log("A soma é" + soma);
+    console.log("A subtração é " + subtracao);
+    console.log("A multiplicação é " + multiplicacao);
+    if  (n2 === 0 ) {                                                   //Utilização de IF para caso o numero seja zero;
+        console.log ("Não da pra dividir por zero");
+
+
+    } else {
+        const divisao = n1 / n2;
+        console.log("A divisão é " + divisao);
+    }
+   }
+   */
+//Atividade 3;
+ /*
+ let n = 10 //I é uma contadora, neste caso o valor dela é 10;
+    while (n >= 1)
+     {            //enquanto, condição ( contadora for maior que 1)  
+        do  {
+        console. log ("contando..." + n);   //contando o número, (contando 1, contando 2)
+        n--;   // O numero vai diminuindo
+        } while (n >= 0);
+    }
+    */
+
+
+//Atividade 4;
+   /*
+   let palavra = prompt ("Digite uma palavra");//guardar as palavras;
+   //  function inverterTexto (palavra) {
+        return palavra.split ('').reverse().join('');  
+    }
+    */
+//Atividade 5 ;
+ // variável para contar caracteres
+ /*
+let palavra = prompt ("digite uma palavra")//variável paara guardar palavra
+  function contartexto(palavra){
+   let nletras=palavra.length   // variável para contar caracteres                          //usando uma "palavra-chave" que conta strings;
+    console.log (nletras);
+  }
+    */
+
+
+//Atividade 6
+ /*
+const carro = {          //declarando um objeto.
+    marca:'Volkswargen ',
+    modelo:'Esmerald',                                   //preenchendo com os dados do novo fusca 2025
+    ano :'2025'
+   
+};
+
+
+
+
+console.log('O carro é da ' +carro.marca + ' Modelo '+ carro.modelo + ' lançado no ano '+ carro.ano)
+ */
+
+
+//Atividade 7;
+/*
+let mensagem= 'Olá!';
+let nome =  prompt ("Qual é o seu nome?");
+function mensagemPersonalizada(nome,mensagem ){
+    console.log(mensagem + nome);
+
+
+
+
+
+
+}
+*/
+// Atividade 8.
+/*
+let n1 =  parseFloat( prompt ("Digite um numero"));  
+let n2 = parseFloat(prompt ("Digite o segundo numero"));
+let n3 = parseFloat(prompt ("Digite o terceiro"));
+                                   
+ function mediatotal(n1, n2, n3){
+    const media=((n1+n2+n3)/3)  //Criando uma variavel que vai somar todos os numeros e dividir  para criar uma média.
+    console.log("A média é " + media);
+ }
+*/
+
+
+// Atividade 9
+/*
+let i = 21 // variável indicadora
+for (;i > 0;){   //para numero maior que zero
+    i--;    // Fechando o código
+    if ( i%3 == 0) {           //se esse número for divisivel (igual a zero) então
+        if (i === 0){              //se o número for 0 é igual, ele não pode ser divisivel por 3
+            console.log(i + " Esse número não é multiplo de 3");    
+        }else{                       // caso o número não seja zero
+            console.log( i + " Esse número aqui é múltiplo por 3");
+        }
+    } else {
+    console.log (i + " Esse número não é multiplo por 3" );     //caso o número não seja divisivel por 3
+}
+} */
+
+
+//Atividade 10
+/*let string = prompt('Digite uma palavra: ')
+
+
+function verificarPalindromo (string) {
+    let reverse = string.split('').reverse('').join('');
+    if (string === reverse) {
+        console.log('Essa palavra é um Palíndromo');
+    }else {
+        console.log('Essa palavra não é um Palíndromo');
+    }
+}
+
+
+*/
+
+
+
+
+//chamar função
+ //verificarPar(n1);
+ //calcular(n1,n2);
+ //inverterTexto(palavra);
+ //contartexto(palavra);
+//mensagemPersonalizada(nome,mensagem);
+//mediatotal(n1,n2,n3);
+//verificarPalindromo (string);
+
+
+
+
+//Lista 2
+
+
+/*
+11.
+
+
+
+
+let n = prompt('Digite um número: ')
+
+
+function verificarSinal(n) {
+    if (n > 0) {
+        console.log('Positivo')
+    } else if (n < 0) {
+        console.log('Negativo')
+    } else if (n == 0) {
+        console.log('Zero')
+    }
+}
+
+
+*/
+
+
+/*
+12.
+
+
+
+
+let temp = parseFloat(prompt('Digite uma temperatura em Celcius'))
+
+
+function converterTemperatura(temp) {
+    const conversao = (temp * 1.8) + 32
+    console.log('A conversão dessa temperatura para Fahrenheit é: ' + conversao)
+}
+
+
+
+
+*/
+
+
+/*
+13.
+
+
+let soma = 0;
+let n = 1;
+
+
+while (n < 101) {
+    soma += n;
+    n++;
+}
+
+
+console.log("A somatória de 1 a 100 é: " + soma);
+
+
+*/
+
+
+/*
+14.
+
+
+let string = prompt('Digite uma frase que contenha a palavra azul: ')
+
+
+function substituirPalavra(string) {
+    let novaString = string.replace('azul', 'vermelho');
+    console.log(novaString)
+}
+
+
+
+
+
+
+*/
+
+
+/*
+15.
+
+
+let string = prompt('Digite uma palavra: ')
+
+
+function verificarTamanho(string) {
+    let qntdLetras = string.length;
+    if (qntdLetras > 10) {
+        console.log('Essa palavra tem mais de 10 caracteres')
+    } else {
+        console.log('Essa palavra não tem mais de 10 caracteres')
+    }
+}
+
+
+*/
+
+
+/*
+16.
+
+
+let frutas = ["Maça", "Uva", "Banana", "Laranja", "Mamão"];
+
+
+console.log(frutas[2]);
+
+
+*/
+
+
+/*
+17.
+
+
+
+
+let nome = prompt('Digite seu nome: ');
+let saudacao = 'Bem vindo(a)! '
+
+
+function saudacaoo(nome, saudacao) {
+    console.log(saudacao + nome);
+}
+
+
+
+
+*/
+
+
+/*
+18.
+
+
+let nota = prompt('Digite sua nota: ')
+
+
+function verificarAprovacao(nota) {
+    if (nota >= 6) {
+        console.log('Você está Aprovado!')
+    } else {
+        console.log('Você está Reprovado.')
+    }
+}
+
+
+
+
+*/
+
+
+/*
+19.
+
+
+for (let i = 0; i <= 30; i++) {
+    if (i % 2 === 0) {
+        console.log(i)
+    }
+}
+
+
+*/
+
+
+/*
+20.
+
+
+let string = prompt('Digite uma palavra: ')
+let i = 0
+let vogais = 'aeiouAEIOU'
+
+
+function contarVogais(string) {
+    for (let letra of string) {
+        if (vogais.includes(letra)) {
+            i++
+        }
+    }
+    console.log(i)
+}
+
+
+*/
+
+
+/*
+21.
+
+
+const n1 = parseFloat(prompt('Digite seu primeiro número: '));
+const n2 = parseFloat(prompt('Digite seu segundo número: '));
+const n3 = parseFloat(prompt('Digite seu terceiro número: '));
+let notas = [n1, n2, n3];
+
+
+let somaDasNotas = 0;
+
+
+function somarArray(notas) {
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i];
+    }
+    console.log(somaDasNotas)
+}
+
+
+*/
+
+
+/*
+22.
+
+
+
+
+let n = parseFloat(prompt('Digite um número: '));
+
+
+function dobrarNumero(n) {
+    dobro = n + n;
+    console.log('O dobro desse número é: ' + dobro);
+}
+
+
+*/
+
+
+/*
+23.
+let n = 2
+
+
+while (n <= 20) {
+    n++
+    if (n % 2 === 0) {
+        console.log(n)
+    }
+}
+*/
+
+
+/*
+24.
+
+
+let string = prompt('Digite uma palavra: ');
+
+
+function capitalizar(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+}
+console.log(capitalizar(string))
+
+
+*/
+
+
+/*
+25.
+let ano = prompt('Digite um ano: ');
+
+
+function anoBissexto(ano) {
+    if (ano % 4 === 0) {
+        console.log('Esse é um ano Bissexto');
+    } else {
+        console.log('Esse não é um ano Bissexto');
+    }
+}
+
+
+*/
+
+
+//chamar função
+//verificarSinal(n);
+//converterTemperatura(temp);
+//anoBissexto(ano);
+//substituirPalavra(string);
+//saudacaoo(nome, saudacao);
+//verificarAprovacao(nota);
+//contarVogais(string);
+//somarArray(notas);
+//dobrarNumero(n);
+//capitalizar(string);
